e95a6ff7b62428f4ca5e1ca1d604d8d197e6bdf1
Robel Seyoum <robelseyoum3@gmail.com>
2020-07-11 10:02:00 +0100

    done with new feature

---
+++ app/src/main/java/com/robelseyoum3/perseusprayer/Constants.kt
@@ -0,0 +1,8 @@
+package com.robelseyoum3.perseusprayer
+
+class Constants {
+
+    companion object {
+        const val ABOUT_ME = "The one and only Roba Zmak"
+    }
+}
--- app/src/main/java/com/robelseyoum3/perseusprayer/MainActivity.kt
+++ app/src/main/java/com/robelseyoum3/perseusprayer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.robelseyoum3.perseusprayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.robelseyoum3.perseusprayer.Constants.Companion.ABOUT_ME
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textId.text = "The one and only Roba Zmak"
+        textId.text = ABOUT_ME
     }
 }
