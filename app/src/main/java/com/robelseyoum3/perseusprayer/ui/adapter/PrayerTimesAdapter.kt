e91d8ed10698f00a0db47deb026aba8990cbb550
Robel Seyoum <robelseyoum3@gmail.com>
2020-07-30 13:42:18 +0100

    fixing issue of multiple view holder

--- app/src/main/java/com/robelseyoum3/perseusprayer/ui/adapter/PrayerTimesAdapter.kt
+++ app/src/main/java/com/robelseyoum3/perseusprayer/ui/adapter/PrayerTimesAdapter.kt
@@ -1,5 +1,6 @@
 package com.robelseyoum3.perseusprayer.ui.adapter
 
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -13,26 +14,62 @@ import com.robelseyoum3.perseusprayer.utils.Constants.Companion.TYPE_FAJARTIME
 import com.robelseyoum3.perseusprayer.utils.Constants.Companion.TYPE_ISHAATIME
 import com.robelseyoum3.perseusprayer.utils.Constants.Companion.TYPE_MAGHRIBTIME
 import com.robelseyoum3.perseusprayer.utils.Constants.Companion.TYPE_SUNRISETIME
+import kotlin.properties.Delegates
 
 class PrayerTimesAdapter constructor(val prayerTimes: MutableList<PrayerTimes>)
     : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when(viewType){
-            TYPE_ASARTIME -> AsarTiimeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.asar_time, parent, false))
-            TYPE_DHUHRTIME -> DhuhrTimeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.dhuhr_time, parent, false))
-            TYPE_FAJARTIME -> FajarTimeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.fajar_time, parent, false))
-            TYPE_ISHAATIME -> IshaaTimeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.ishaa_time, parent, false))
-            TYPE_MAGHRIBTIME -> IshaaTimeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.maghrib_time, parent, false))
-            else ->  IshaaTimeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.sunrise_time, parent, false))
+        return when (viewType) {
+            TYPE_ASARTIME -> AsarTiimeViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.asar_time,
+                    parent,
+                    false
+                )
+            )
+            TYPE_DHUHRTIME -> DhuhrTimeViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.dhuhr_time,
+                    parent,
+                    false
+                )
+            )
+            TYPE_FAJARTIME -> FajarTimeViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.fajar_time,
+                    parent,
+                    false
+                )
+            )
+            TYPE_ISHAATIME -> IshaaTimeViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.ishaa_time,
+                    parent,
+                    false
+                )
+            )
+            TYPE_MAGHRIBTIME -> IshaaTimeViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.maghrib_time,
+                    parent,
+                    false
+                )
+            )
+            else -> IshaaTimeViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.sunrise_time,
+                    parent,
+                    false
+                )
+            )
         }
     }
 
     override fun getItemCount(): Int = prayerTimes.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when(getItemViewType(position)){
+        when (getItemViewType(position)) {
             TYPE_ASARTIME -> (holder as AsarTiimeViewHolder).bind(prayerTimes[position])
             TYPE_DHUHRTIME -> (holder as DhuhrTimeViewHolder).bind(prayerTimes[position])
             TYPE_FAJARTIME -> (holder as FajarTimeViewHolder).bind(prayerTimes[position])
@@ -43,15 +80,37 @@ class PrayerTimesAdapter constructor(val prayerTimes: MutableList<PrayerTimes>)
     }
 
 
-
     override fun getItemViewType(position: Int): Int {
-        return when(position){
-            prayerTimes[position].->  TYPE_ASARTIME
+
+        var intViewType: Int = 0
+        when {
+            TextUtils.isEmpty(prayerTimes[position].mAsr) -> {
+                intViewType = TYPE_ASARTIME
+            }
+            TextUtils.isEmpty(prayerTimes[position].mZuhr) -> {
+                intViewType =  TYPE_DHUHRTIME
+            }
+            TextUtils.isEmpty(prayerTimes[position].mFajr) -> {
+                intViewType = TYPE_DHUHRTIME
+            }
+            TextUtils.isEmpty(prayerTimes[position].mZuhr) -> {
+                intViewType =   TYPE_FAJARTIME
+            }
+            TextUtils.isEmpty(prayerTimes[position].mISHA) -> {
+                intViewType =   TYPE_ISHAATIME
+            }
+            TextUtils.isEmpty(prayerTimes[position].mMaghrib) -> {
+                intViewType =   TYPE_MAGHRIBTIME
+            }
+            TextUtils.isEmpty(prayerTimes[position].mSunrise) -> {
+                intViewType =  TYPE_SUNRISETIME
+            }
         }
+        return intViewType
     }
 
-
 }
+
 //    override fun onBindViewHolder(holder: PrayerTimesViewHolder, position: Int) {
 //
 //        holder.fajarTime.text = prayerTimes[position].mFajr
