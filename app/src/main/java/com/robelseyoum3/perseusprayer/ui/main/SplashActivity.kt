cda5edce5a67b0a652a76ac222a604b53f13d632
Robel Seyoum <robelseyoum3@gmail.com>
2020-08-13 15:42:39 +0100

    adding splash screen and play store icon

---
+++ app/src/main/java/com/robelseyoum3/perseusprayer/ui/main/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.robelseyoum3.perseusprayer.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.robelseyoum3.perseusprayer.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashTime = 2000L //3 second
+    private lateinit var myHandler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        myHandler = Handler()
+
+        myHandler.postDelayed({
+            goToMainActivity()
+        }, splashTime)
+    }
+
+    private fun goToMainActivity() {
+        val mainActivityIntent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(mainActivityIntent)
+        finish()
+    }
+}
